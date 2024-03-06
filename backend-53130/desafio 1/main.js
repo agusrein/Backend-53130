5b8349140b60a642d30889e5f21e762570638903
agusrein <agusreinaldi10@gmail.com>
2024-03-06 18:56:38 -0300

    Correcciones del codigo por mala legibilidad

--- backend-53130/desafio 1/main.js
+++ backend-53130/desafio 1/main.js
@@ -2,6 +2,7 @@ class ProductManager {
     static id = 0;
     constructor() {
         this.products = [];
+        this.path = './products.txt';
     }
 
     addProduct(title, description, price, thumbnail, code, stock) {
@@ -34,19 +35,12 @@ class ProductManager {
 
 const products = new ProductManager;
 
-
-products.getProducts();
-
 products.addProduct('Producto prueba uno', 'Descripcion del producto uno' , 2400,'./public/img/foto1.png', '08uw9', 200);
-products.addProduct('Producto prueba dos', 'Descripcion del producto dos' , 9400,'./public/img/foto1.png', '08349', 200);
-
-products.getProducts();
-
 products.addProduct('Producto prueba dos', 'Descripcion del producto dos' , 3500,'./public/img/foto1.png', '08urww'); // Producto con falta de información
+products.addProduct('Producto prueba tres', 'Descripcion del producto tres' , 9400,'./public/img/foto1.png', '08349', 200);
+products.addProduct('Producto prueba cuatro', 'Descripcion del producto uno' , 2400,'./public/img/foto1.png', '08uw9', 200); //Producto con mismo codigo
 
-products.addProduct('Producto prueba cuatro', 'Descripcion del producto cuatro' , 2400,'./public/img/foto1.png', '08uw9', 200); //Producto con mismo codigo
-
-
+// products.getProducts();
 products.getProductById(1);
 products.getProductById(4);
 
