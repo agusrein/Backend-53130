8db4d1078d17232f064fc974024df7ef9a37e4c2
agusrein <agusreinaldi10@gmail.com>
2024-03-13 19:46:36 -0300

    Correccion de detalles 2da entrega

--- backend-53130/desafio-2/ProductManager.js
+++ backend-53130/desafio-2/ProductManager.js
@@ -6,68 +6,71 @@ class ProductManager {
     constructor(path) {
         this.products = [];
         this.path = path;
+        this.readFile();
+    }
+
+    readFile() {
+            const data =  fs.readFileSync(this.path, "utf-8");
+            if (data) {
+                this.products = JSON.parse(data);
+            };
+            return this.products;
     }
 
     async addProduct(title, description, price, thumbnail, code, stock) {
         const product = { title, description, price, thumbnail, code, stock };
 
-        const data = await fs.promises.readFile(this.path,"utf-8");
-        if(data){this.products = JSON.parse(data)};
+        this.readFile();
 
-            if (this.products.find(e => e.code === product.code)) {
-                console.log(`ERROR el producto con el codigo ${product.code}, ya existe.`)
-                return;
-            }
-            else if (!product.title || !product.description || !product.price || !product.thumbnail || !product.stock) {
-                console.log('ERROR debe llenar todos los datos para ingresar un artículo');
-                return;
-            }
+        if (this.products.find(e => e.code === product.code)) {
+            console.log(`ERROR el producto con el codigo ${product.code}, ya existe.`)
+            return;
+        }
+        else if (!product.title || !product.description || !product.price || !product.thumbnail || !product.stock) {
+            console.log('ERROR debe llenar todos los datos para ingresar un artículo');
+            return;
+        }
 
-            else this.products.push({ ...product, id: ++ProductManager.id });
-    
-            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
- 
+        else this.products.push({ ...product, id: ++ProductManager.id });
+
+        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
+        await this.readFile();
     }
 
 
 
     getProducts() {
-        const data = fs.readFileSync(this.path,"utf-8");
-        data ? this.products = JSON.parse(data) : this.products;
         return this.products;
     }
 
 
 
-    async getProductById(id) {
-        try {
-            const data = await fs.promises.readFile(this.path, 'utf-8');
-            this.products = JSON.parse(data)
+    getProductById(id) {
             const productFound = this.products.find(e => e.id === id);
-            productFound ? console.log(`Producto ${id} encontrado :`, productFound) : console.log(`ERROR Not Found : ${id}`);
-        }
-        catch {
-            (error => console.log(error))
-        }
+            if(productFound){
+                console.log(`Producto ${id} encontrado :`)
+                return productFound;
+            }
+            return `ERROR Not Found : ${id}`
+
     }
 
 
     async updateProduct(id, { property, value }) {
         try {
-            const data = await fs.promises.readFile(this.path, 'utf-8');
-            this.products = JSON.parse(data)
             const i = this.products.findIndex(e => e.id === id);
 
             //Corroboro que la propiedad que se pasa como parámetro se encuentre y no se me agregue al objeto como una nueva
 
             const existingProperty = this.products.some(e => e.hasOwnProperty(property));
-            
-            
+
+
             // Si existe el indice del id pasado como parámetro, y la propiedad tambien existe y es distinta a id para ésta no se modifique, entonces proceder.
 
             if (i >= 0 && existingProperty && property != id) {
                 this.products[i] = { ...this.products[i], [property]: value }
                 await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
+                await this.readFile();
                 console.log(`La propiedad ${property} del producto ${id} se ha modificado correctamente.`)
             }
             else { console.log(`ERROR Not Found : ${id}`) }
@@ -81,13 +84,13 @@ class ProductManager {
 
     async deleteProduct(id) {
         try {
-            const data = await fs.promises.readFile(this.path, 'utf-8');
-            this.products = JSON.parse(data)
             const productFound = this.products.find(e => e.id === id);
             if (productFound) {
                 this.products = this.products.filter(object => object !== productFound); //Filtro el array para descartar el producto a eliminar.
                 console.log(`El producto ${id} se ha eliminado.`)
-                return await fs.promises.writeFile(this.path, JSON.stringify(this.products)) // Piso el antiguo documento y guardo el nuevo array sin el elemento eliminado.
+                await fs.promises.writeFile(this.path, JSON.stringify(this.products)) // Piso el antiguo documento y guardo el nuevo array sin el elemento eliminado.
+                await this.readFile(); 
+                return;
             }
             else { console.log(`ERROR Not Found : ${id}`) }
         }
@@ -105,20 +108,21 @@ class ProductManager {
 // //2)
 // console.log(products.getProducts());
 
-// //3) 
+// // //3)
 // products.addProduct('producto prueba', 'Este es un producto prueba',200, 'sin imagen', 'abc123',25);
 // products.addProduct('producto prueba', 'Este es un producto prueba',200, 'sin imagen', 'abc234',25);
 // products.addProduct('producto prueba', 'Este es un producto prueba',200, 'abc123',25);
 
-// //4)
+// // // // //4)
 // console.log(products.getProducts());
 
 // //5)
-// products.getProductById(1);
-// products.getProductById(5);
+// console.log(products.getProductById(1));
+// console.log(products.getProductById(5));
 
 //6)
-// products.updateProduct(2,{property:'code',value:'codigocambiado'});
+// products.updateProduct(2,{property:'code',value:'locambie'});
+// console.log(products.getProductById(2));
 
 //7)
 // products.deleteProduct(2);
