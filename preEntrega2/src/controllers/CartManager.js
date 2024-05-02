8227e2106a3f8ff9af3c072576ea7e01c2fefcfa
agusrein <agusreinaldi10@gmail.com>
2024-05-02 12:30:26 -0300

    Agregado de metodos delete y modificaciones por error en conexiones

--- preEntrega2/src/controllers/CartManager.js
+++ preEntrega2/src/controllers/CartManager.js
@@ -5,7 +5,7 @@ class CartManager {
 
     async createCart() {
         try {
-            const newCart = new CartModel({products: []});
+            const newCart = new CartModel({ products: [] });
             await newCart.save();
             return { status: true, message: `Carrito Creado`, cart: newCart }
 
@@ -32,7 +32,7 @@ class CartManager {
             const cartFound = await CartModel.findById(id);
             if (cartFound) {
                 const existingProduct = cartFound.products.find(e => e.product.toString() == productId);
-                existingProduct ? existingProduct.quantity += quantity : cartFound.products.push({ product: productId, quantity});
+                existingProduct ? existingProduct.quantity += quantity : cartFound.products.push({ product: productId, quantity });
                 cartFound.markModified("products");
                 await cartFound.save();
                 return { status: true, message: 'Producto Agregado' }
@@ -45,6 +45,48 @@ class CartManager {
             return { status: false, message: `LO SENTIMOS, HA OCURRIDO UN ERROR ${error}` }
         }
 
+    }
+
+    async deleteProduct(id, productId, quantity = 1) {
+        try {
+            const cartFound = await CartModel.findById(id);
+            if (cartFound) {
+                const existingProduct = cartFound.products.find(e => e.product.toString() == productId);
+                const existingProductIndex = cartFound.products.findIndex(e => e.product.toString() == productId);
+                if (existingProduct) {
+                    existingProduct.quantity -= quantity;
+                    if (existingProduct.quantity == 0) {
+                        cartFound.products.splice(existingProductIndex, 1)
+                    }
+                    cartFound.markModified("products");
+                    await cartFound.save();
+                    return { status: true, message: 'Producto Eliminado' }
+                } else {
+                    return { status: false, message: `ERROR Not Found : ${productId}` }
+                }
+            }
+            else {
+                return { status: false, message: `ERROR Not Found : ${id}` }
+            }
+        } catch (error) {
+            return { status: false, message: `LO SENTIMOS, HA OCURRIDO UN ERROR ${error}` }
+        }
+
+    }
+
+    async emptyCart(id){
+        try {
+            const cartFound = await CartModel.findById(id);
+            if (cartFound) {
+                cartFound.products = []
+                cartFound.__v = 0;
+                await cartFound.save();
+                return { status: true, message: 'Carrito Eliminado:'};
+            }
+            return { status: false, message: `ERROR Not Found : ${id}` }
+        } catch (error) {
+            return { status: false, message: `LO SENTIMOS, HA OCURRIDO UN ERROR ${error}` }
+        }
     }
 
 }
--- preEntrega2/src/routes/carts.router.js
+++ preEntrega2/src/routes/carts.router.js
@@ -1,9 +1,9 @@
 const CartManager = require('../controllers/CartManager');
-const ProductManager = require('../controllers/ProductManager');
 const express = require('express');
+const cartsModel = require('../models/carts.model');
+
 const router = express.Router();
 
-const productManager = new ProductManager()
 const cartManager = new CartManager();
 
 router.post('/carts', async (request, response) => {
@@ -36,19 +36,32 @@ router.post('/carts/:cid/products/:pid', async (request, response) => {
     const cid = request.params.cid;
     const quantity = request.body.quantity;
     try {
-        const totalProducts = await productManager.getProducts();
-        if (totalProducts.status) {
-            const products = totalProducts.products;
-            const productToAdd = products.find(e => e._id == pid)
-            if (productToAdd) { //Corroboro que el id de producto que quiero agregar exista 
-                const updateCart = await cartManager.addProductToCart(cid, pid, quantity);
-                updateCart.status ? response.status(200).send({ message: updateCart.message }) : response.status(404).send({ message: updateCart.message })
-            }
-            else{
-                response.status(404).send({message: 'El producto que intenta agregar no existe'});
-            }
+        const updateCart = await cartManager.addProductToCart(cid, pid, quantity);
+            updateCart.status ? response.status(200).send({ message: updateCart.message }) : response.status(404).send({ message: updateCart.message })
+    
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+})
+
+router.delete('/carts/:cid/products/:pid', async (request,response) =>{
+    const pid = request.params.pid;
+    const cid = request.params.cid;
+    let quantity;
+    try {
+        const productDelete = await cartManager.deleteProduct(cid,pid,quantity);
+        if(productDelete.status){
+            return response.status(200).send({ message: productDelete.message })
         }
-        
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+})
+router.delete('/carts/:cid', async (request, response)=>{
+    const cid = request.params.cid;
+    try {
+        const emptyCart = await cartManager.emptyCart(cid)
+        emptyCart.status ? response.status(200).send({ message: emptyCart.message}) : response.status(404).send({ message: emptyCart.message });
     } catch (error) {
         response.status(500).send(error.message);
     }
