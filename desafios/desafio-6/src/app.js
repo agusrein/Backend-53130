b3135cbf80da7b8c5e34e30f564298fe22bdc447
agusrein <agusreinaldi10@gmail.com>
2024-05-07 13:19:23 -0300

    Preparando organizacion de contenido desafio-6

---
+++ desafios/desafio-6/src/app.js
@@ -0,0 +1,82 @@
+const productsRouter = require('./routes/products.router');
+const cartsRouter = require('./routes/carts.router');
+const viewsRouter = require('./routes/views.router');
+const express = require('express');
+const app = express();
+const PUERTO = 8080;
+const exphbs = require('express-handlebars');
+const socket = require('socket.io');
+require('./database.js')
+const ProductManager = require('./controllers/ProductManager');
+const productManager = new ProductManager()
+
+
+
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("./src/public"));
+
+//ROUTES
+app.use('/api', productsRouter);
+app.use('/api', cartsRouter);
+app.use('/', viewsRouter);
+
+
+//HANDLEBARS
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+
+
+const httpServer = app.listen(PUERTO, () => {
+    console.log(`Escuchando puerto http//localhost:${PUERTO}`);
+})
+
+
+//WEBSOCKET
+const io = new socket.Server(httpServer);
+const MessageModel = require('./models/messages.model.js');
+
+io.on("connection", (socket) =>{
+    console.log('usuario conectado');
+    socket.on("message", async data =>{
+        await MessageModel.create(data);
+
+        const messages = await MessageModel.find();
+        io.sockets.emit("message", messages);
+    })
+})
+
+// io.on('connection',async (socket)=>{
+//     socket.emit("products", await productManager.getProducts());
+//     socket.on("deleteProduct" , async (id) =>{
+//         await productManager.deleteProduct(id);
+//         socket.emit("products", await productManager.getProducts());
+//     })
+//     socket.on("addProduct", async (product) =>{
+//         const result = await productManager.addProduct(
+//             product.title,
+//             product.description,
+//             product.price,
+//             product.thumbnail, 
+//             product.code,
+//             product.stock,
+//             product.status,
+//             product.img,
+//             product.category
+//         );
+//         if (result.status) {
+//             console.log(result.message); 
+//             socket.emit('products', await productManager.getProducts());
+//         } else {
+//             console.error(result.message); 
+//         }
+//     })
+// })
+
+
+
+
---
+++ desafios/desafio-6/src/routes/views.router.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const ProductManager = require('../controllers/ProductManager');
+const productsManager = new ProductManager();
+const CartManager = require('../controllers/CartManager');
+const cartManager = new CartManager();
+
+router.get('/products', async (request, response) => {
+    try {
+        let page = parseInt(request.query.page) || 1;
+        let limit = parseInt(request.query.limit) || 10;
+        let query = request.query.category || null;
+        let sort = parseInt(request.query.sort) || 1;
+        const products = await productsManager.getProducts({ page, limit, query, sort });
+
+        const finalProducts = products.docs.map(e => {
+            const { _id, ...rest } = e.toObject();
+            return rest;
+        })
+
+        response.render('home', {
+            products: finalProducts,
+            hasPrevPage: products.hasPrevPage,
+            hasNextPage: products.hasNextPage,
+            prevPage: products.prevPage,
+            nextPage: products.nextPage,
+            currentPage: products.page,
+            totalPages: products.totalPages,
+            prevLink: products.hasPrevPage ? `/products?limit=${limit}&page=${products.prevPage}&sort=${sort}&query=${query}` : null,
+            nextLink: products.hasNextPage ? `/products?limit=${limit}&page=${products.nextPage}&sort=${sort}&query=${query}` : null,
+        });
+    } catch (error) {
+        response.status(500).send({ message: error.message });
+    }
+
+})
+
+
+router.get("/realtimeproducts", (request, response) => {
+    try {
+        response.render('realtimeproducts')
+    } catch (error) {
+        response.status(500).send({ message: error.message });
+    }
+})
+
+router.get("/chat", async (req, res) => {
+    res.render("messages");
+})
+
+router.get("/carts/:cid", async (req, res) => { //6633aeb7cab032e9126855da ID TEST
+    try {
+        const cid = req.params.cid;
+        const cart = await cartManager.getProductsByCart(cid);
+        console.log(cart)
+        if(cart.status){
+            const products = cart.cart.products.map(e => ({
+                product: e.product.toObject(),
+                quantity: e.quantity
+             }));
+             res.render('carts', {cart: products} )
+        }
+        else{res.status(404).send({ message: cart.message });}
+        
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+
+
+})
+
+module.exports = router;
