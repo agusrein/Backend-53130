f17e8840aeda4e7e34e44d9d2a05aedcc4091588
agusrein <agusreinaldi10@gmail.com>
2024-06-04 21:54:21 -0300

    Comenzando desafio 9 , configuracion de variables de entorno

---
+++ desafios/desafio-9/src/models/products.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const productsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    thumbnail: {
+        type: [String],
+    },
+    code: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        required: true
+    },
+    img:{
+        type: String,
+    },
+    category: {
+        type: String,
+        required: true
+    }
+});
+
+productsSchema.plugin(mongoosePaginate);
+
+const productsModel = mongoose.model("products", productsSchema);
+module.exports = productsModel;
---
+++ desafios/desafio-9/src/routes/views.router.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const ProductManager = require('../controllers/ProductManager');
+const productsManager = new ProductManager();
+const CartManager = require('../controllers/CartManager');
+const cartManager = new CartManager();
+const passport = require('passport');
+const {jwtAuth} = require('../config/passport.config.js')
+
+router.get('/products',passport.authenticate('jwt', { session: false }), async (request, response) => {
+    try {
+        let page = parseInt(request.query.page) || 1;
+        let limit = parseInt(request.query.limit) || 10;
+        let query = request.query.category || null;
+        let sort = parseInt(request.query.sort) || 1;
+        const products = await productsManager.getProducts({ page, limit, query, sort });
+
+        const finalProducts = products.docs.map(e => {
+            const { _id, ...rest } = e.toObject();
+            return rest;
+        })
+
+        response.render('home', {
+            user: request.user.user,
+            products: finalProducts,
+            hasPrevPage: products.hasPrevPage,
+            hasNextPage: products.hasNextPage,
+            prevPage: products.prevPage,
+            nextPage: products.nextPage,
+            currentPage: products.page,
+            totalPages: products.totalPages,
+            prevLink: products.hasPrevPage ? `/products?limit=${limit}&page=${products.prevPage}&sort=${sort}&query=${query}` : null,
+            nextLink: products.hasNextPage ? `/products?limit=${limit}&page=${products.nextPage}&sort=${sort}&query=${query}` : null,
+        });
+    } catch (error) {
+        response.status(500).send({ message: error.message });
+    }
+
+})
+
+
+router.get("/realtimeproducts", (request, response) => {
+    try {
+        response.render('realtimeproducts')
+    } catch (error) {
+        response.status(500).send({ message: error.message });
+    }
+})
+
+router.get("/chat", async (req, res) => {
+    res.render("messages");
+})
+
+router.get("/carts/:cid", async (req, res) => { //6633aeb7cab032e9126855da ID TEST
+    try {
+        const cid = req.params.cid;
+        const cart = await cartManager.getProductsByCart(cid);
+        console.log(cart)
+        if(cart.status){
+            const products = cart.cart.products.map(e => ({
+                product: e.product.toObject(),
+                quantity: e.quantity
+             }));
+             res.render('carts', {cart: products} )
+        }
+        else{res.status(404).send({ message: cart.message });}
+        
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+
+
+})
+
+router.get("/login", (req,res) =>{
+res.render("login")
+})
+
+router.get("/register", (req,res) =>{
+    res.render("register")
+})
+
+router.get("/profile", jwtAuth, (req, res) => {
+    return res.render("profile", { user: req.user.user });
+});
+module.exports = router;
